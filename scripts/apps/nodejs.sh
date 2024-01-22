eb9396a4cb37d40a5bf20babfb784ae090f7ba49
Genji Ohara <oharagenji1024@gmail.com>
2024-01-22 13:39:55 +0900

    Add command to install curl before installing node.js in scripts/apps/nodejs.sh

--- scripts/apps/nodejs.sh
+++ scripts/apps/nodejs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install curl if both curl and wget are not installed
+if ! [ -x "$(command -v curl)" ] && ! [ -x "$(command -v wget)" ]; then
+  sudo apt update && sudo apt install -y curl
+fi
+
+# Install Node.js
 sudo apt update && sudo apt install -y nodejs npm
 sudo npm install n -g
 sudo n stable
