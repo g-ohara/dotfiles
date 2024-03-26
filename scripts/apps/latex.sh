f3fc9b13eed8a330e82971d616a2d51c4214ec90
Genji Ohara <oharagenji1024@gmail.com>
2024-03-26 14:51:31 +0900

    fix: remove redundant scripts

--- scripts/apps/latex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-
-# install TexLive
-sudo apt install -y texlive-full
-
-# install xdg-util & evince to view PDF file on vimtex
-sudo apt install -y xdg-utils evince
--- scripts/system/mount_google_drive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository ppa:alessandro-strada/ppa
-sudo apt update && sudo apt install -y google-drive-ocamlfuse
-google-drive-ocamlfuse
-mkdir ~/GoogleDrive
-google-drive-ocamlfuse ~/GoogleDrive
-
