f4cb1d2c184e7b66fab16413533bd4445a7b0e3f
Genji Ohara <oharagenji1024@gmail.com>
2024-05-27 12:42:42 +0900

    feat: add script to install some external apps for neovim

--- scripts/apps/neovim.sh
+++ scripts/apps/neovim.sh
@@ -2,3 +2,6 @@
 
 # Uninstall Neovim installed via apt and install it via snap
 sudo apt purge --autoremove -y neovim && sudo snap install nvim --classic
+
+# Install external tools for Neovim
+sudo apt update && sudo apt install -y ripgrep xclip
