7e7ff88a01e82b625c31cd8424248e7d65c37993
Genji Ohara <oharagenji1024@gmail.com>
2024-06-18 17:04:21 +0900

    feat: add neovim column line settings for python

--- .config/nvim/lua/design.lua
+++ .config/nvim/lua/design.lua
@@ -8,6 +8,18 @@ vim.opt.breakindent = true
 -- Set column lign at 80 characters
 vim.opt.colorcolumn = "80"
 
+-- Set column lign at 72 and 79 characters for Python
+vim.api.nvim_create_autocmd(
+    "FileType",
+    {
+        group = vim.api.nvim_create_augroup("local_colorcolumn", {}),
+        pattern = { "python" },
+        callback = function()
+            vim.opt.colorcolumn = "72,79"
+        end
+    }
+)
+
 -- Display underline for cursor line
 vim.opt.cursorline = true
 
