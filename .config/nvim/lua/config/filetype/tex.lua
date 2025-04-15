355ea71e8fa40e38be00777e846929e1fb13aae1
Genji Ohara <oharagenji1024@gmail.com>
2025-04-15 15:19:44 +0900

    feat(neovim): open pdf on saving tex file

--- .config/nvim/lua/config/filetype/tex.lua
+++ .config/nvim/lua/config/filetype/tex.lua
@@ -28,3 +28,15 @@ lspconfig.texlab.setup {
     },
   },
 }
+
+-- Open the target PDF on save
+vim.api.nvim_create_autocmd(
+  "BufWritePost",
+  {
+    pattern = { "*.tex", '*.lhs' },
+    group = vim.api.nvim_create_augroup("AutoFormat", {}),
+    callback = function()
+      vim.cmd("silent !evince %:r.pdf & ")
+    end,
+  }
+)
