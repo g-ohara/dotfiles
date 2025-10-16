bac9e05eaa396fe9181d515fbf8b5be30245869c
Genji Ohara <oharagenji1024@gmail.com>
2025-10-16 11:21:00 +0900

    feat: define keybinding for jump-to-definition

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -6,3 +6,18 @@ vim.lsp.enable('pyright')
 vim.lsp.enable('ruff')
 vim.lsp.enable('texlab')
 vim.lsp.enable('ts_ls')
+
+-- LSP keybindings
+vim.api.nvim_create_autocmd(
+  "LspAttach",
+  {
+    callback = function(event)
+      local opts = {
+        noremap = true,
+        silent = true,
+        buffer = event.buf,
+      }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    end,
+  }
+)
