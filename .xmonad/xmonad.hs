be946f28f8dbee530244fe7039eb36c63513caf0
Genji Ohara <oharagenji1024@gmail.com>
2026-01-20 19:22:10 +0900

    feat(xmonad): set up xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,9 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig (additionalKeysP)
 
 main :: IO ()
-main = xmonad $ def `additionalKeysP` myKeys
+main = xmonad =<< xmobar (def `additionalKeysP` myKeys)
  where
   myKeys =
     [ ("<Print>", spawn "maim -s | xclip -selection clipboard -t image/png")
